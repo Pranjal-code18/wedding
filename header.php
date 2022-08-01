47abea6e3229c4dd528ae5c6eef7cb863d325e92
Pranjal Bansal <pranjalbansal589@gmail.com>
2022-08-01 22:34:00 +0530

    first commit

---
+++ header.php
@@ -0,0 +1,16 @@
+<section class="header">
+
+   <a href="home.php" class="logo">Event Planner</a>
+
+   <nav class="navbar">
+      <a href="home.php">Home</a>
+      <a href="about.php">About</a>
+      <a href="portfolio.php">Portfolio</a>
+      <a href="pricing.php">Pricing</a>
+      <a href="contact.php">Contact</a>
+      <a href="./login/index.php">Logout</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
